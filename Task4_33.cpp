7db77c6fac919fb997687de842c4cb1f0dbd0ecb
Johnathan25 <168485693+Johnathan25@users.noreply.github.com>
2024-12-22 17:54:09 +0200

    Update Task4_33.cpp

--- Task4_33.cpp
+++ Task4_33.cpp
@@ -1,19 +1,16 @@
-// ConsoleApplication10.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <string>
 #include <math.h>
 using namespace std;
-string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-string decription(string Str,int a,int b)
+string alphabet = " ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+string decription(string Str, int a, int b)   //"WEKWFCETNVDXZMNJDRV"       EXPECTED OUTPUT "TFBTAPFIMYKOERMGKSY"
 {
-    int c=0;
-    int p=0;
+    int c = 0;
+    int p = 0;
     int a_inv;
-    for (int k = 0;k < 26;k++)                  //find a inverse
+    for (int k = 0;k < 27;k++)
     {
-        if ((a * k) % 26 == 1)
+        if ((a * k) % 27 == 1)
         {
             a_inv = k;
             break;
@@ -24,28 +21,24 @@ string decription(string Str,int a,int b)
     string massage = "";
     for (int i = 0;i < Str.length();i++)
     {
-        if(Str[i]>=65&&Str[i]<=90)       //if the letter is capital find its dycreption
+        if (Str[i] >= 97 && Str[i] <= 122)
+            Str[i] = Str[i] - 32;
+        for (int j = 0;j < alphabet.length();j++)       //find index of each character in dycrebted text
         {
-            c=Str[i]-65;
-            p = (a_inv * (c - b)) % 26;
-
-            if (p > -25 && p < 0)
-                p += 26;
-            p+=65;
-            massage = massage + (char)p;
+            if (Str[i] == alphabet[j])
+            {
+                c = j;
+                break;
+            }
+            else
+                continue;
         }
-        if(Str[i]>=97&&Str[i]<=122)     //if the letter is small find its dycreption
-        {
-            c=Str[i]-97;
-            p = (a_inv * (c - b)) % 26;
+        p = (a_inv * (c - b)) % 27;
 
-            if (p > -25 && p < 0)
-                p += 26;
-            p+=97;
-            massage = massage + (char)p;
+        if (p > -26 && p < 0)
+            p += 27;
 
-        }
-
+        massage = massage + alphabet[p];
     }
 
     return massage;
